e0de5177ec96d3748a09332494d8d9700dab0c93
Leonid Fedorov <flyonch@MacBook-Air-Leonid.local>
2022-05-20 11:14:51 +0500

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WebSock from './WebSock';
+
+function App() {
+
+  return (
+    <div>
+      <WebSock />
+    </div>
+  )
+}
+
+export default App;
